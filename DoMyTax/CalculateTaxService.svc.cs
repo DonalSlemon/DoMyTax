329d8ca0ac1097353555f94558df33ca198158a4
DonalSlemon <donal.slemon@gmail.com>
2018-07-03 21:34:30 +0200

    Add project files.

---
+++ DoMyTax/CalculateTaxService.svc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DoMyTax
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalculateTaxService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CalculateTaxService.svc or CalculateTaxService.svc.cs at the Solution Explorer and start debugging.
+    public class CalculateTaxService : ICalculateTaxService
+    {
+        public decimal Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public decimal Divide(int bignumber, int smallnumber)
+        {
+            return bignumber / smallnumber;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public decimal Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public decimal Subtract(int bignumber, int smallnumber)
+        {
+            return bignumber - smallnumber;
+        }
+    }
+}
---
+++ DoMyTax/ICalculateTaxService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DoMyTax
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculateTaxService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+        [OperationContract]
+        decimal Add(int number1, int number2);
+
+        [OperationContract]
+        decimal Subtract(int bignumber, int smallnumber);
+
+        [OperationContract]
+        decimal Multiply(int number1, int number2);
+
+        [OperationContract]
+        decimal Divide(int bignumber, int smallnumber);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
